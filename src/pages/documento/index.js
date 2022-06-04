d6263dd5ad844336f5f6646b8abbf2fb580d05a7
CesarOliiveira <cesaroliiveira8@gmail.com>
2022-06-04 17:28:33 -0300

    Arrumando Documentação e testando Site

--- src/pages/documento/index.js
+++ src/pages/documento/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../../components/header/index'
+import Footer from '../../components/footer/index'
 import './documentacao.css'
 // Página da Documentação do TCC - ConnTec
 function Documento(){
@@ -8,25 +9,28 @@ function Documento(){
             <Header/>
             <main>
                 <div className='wrap-content-documentation'>
-                    <div className='title-documentation'>
-                        <h1>
-                            Documentação
-                        </h1>
-                    </div>
-                    <div class='description-documentation'>
-                        <p>
-                            Em nossa documentação abordaremos nossos Problemas, Objetivos, Solução, Funcionalidaes, Metodos usados para a conclusão do Projeto.
-                        </p>
-                    </div>
-                    <div className="section-button">
-                            <a  className="button-download" href='#'>
-                                <div className="button">
-                                    <span>Download</span>
-                                </div>
-                            </a>
+                    <div className='content-documentation'>
+                        <div className='title-documentation'>
+                            <h1>
+                                Documentação
+                            </h1>
+                        </div>
+                        <div class='description-documentation'>
+                            <p>
+                                Em nossa documentação abordaremos nossos Problemas, Objetivos, Solução, Funcionalidaes, Metodos usados para a conclusão do Projeto.
+                            </p>
+                        </div>
+                        <div className="section-button">
+                                <a  className="button-download" href='#'>
+                                    <div className="button">
+                                        <span>Download</span>
+                                    </div>
+                                </a>
+                        </div>
                     </div>
                 </div>
             </main>
+            <Footer/>
         </>
     );
 };
