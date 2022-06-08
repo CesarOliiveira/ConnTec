d4a869e4e8e56ff8165d1ae8df90b10d6e88de57
CesarOliiveira <cesaroliiveira8@gmail.com>
2022-06-08 14:48:50 -0300

    Colocando a tela de post no painel

--- src/pages/painel/index.js
+++ src/pages/painel/index.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, {useRef} from 'react';
+import {Editor} from '@tinymce/tinymce-react';
+import './painel.css';
+import '../../components/MenuPainel';
 
-import './painel.css'; 
 import {ImHome} from 'react-icons/im';
 import {BsGearFill} from 'react-icons/bs';
 import {TiEdit} from 'react-icons/ti';
 import {FaUserAlt} from 'react-icons/fa';
 import {FaPaste} from 'react-icons/fa';
 import {IoMdExit} from 'react-icons/io';
+import { toBePartiallyChecked } from '@testing-library/jest-dom/dist/matchers';
 
-export default function MenuPainel() {
+export default function Painel() {
+    const editorRef = useRef(null);
+    const log = () => {
+        if (editorRef.current) {
+            console.log(editorRef.current.getContent());
+        }
+    };
     return(
        <>
+            
+
             <header className='header-painel'>
                 <div className="hdbox">
                     <h2>Painel</h2>
@@ -29,7 +40,7 @@ export default function MenuPainel() {
                 <div className="content-menubar">
                     <div className="menubar">
                         <ul>
-                            <li><a href='#'><ImHome className='faicon'/>Início</a></li>
+                            <li><a href='/'><ImHome className='faicon'/>Início</a></li>
                             <li><a href='#'><BsGearFill className='faicon'/>Config</a></li>
                         </ul>
                     </div>
@@ -54,6 +65,51 @@ export default function MenuPainel() {
                             </li>
                         </ul>
                     </div>
+                </div>
+                <div className='content-box-options'>
+                    <h1 style={{color: 'black', marginBottom: '12px', fontWeight: 600}}>Titulo para Postagem:</h1>
+                    <input 
+                    type='text' 
+                    placeholder='Titulo da Postagem.' 
+                    style={{
+                        width: '700px',
+                        height: '40px',
+                        borderRadius: '5px',
+                        marginBottom: '30px',
+                    }}/>
+                    <h1 style={{color: 'black', marginBottom: '12px', fontWeight: 600}}>Conteudo da Postagem:</h1>
+                    <Editor
+                        onInit={(evt, editor) => editorRef.current = editor}
+                        initialValue="<p>This is the initial content of the editor.</p>"
+                        apiKey='lcxq3g3d1gmhzadd8lz229tw8cu24wvhotcnkc4diz3tthlg'
+                        init={{
+                            height: 500,
+                            width: 900,
+                            menubar: false,
+                            plugins: [
+                                'advlist autolink lists link image charmap print preview anchor',
+                                'searchreplace visualblocks code fullscreen',
+                                'insertdatetime media table paste code help wordcount'
+                            ],
+                            toolbar: 'undo redo | formatselect | ' +
+                            'bold italic backcolor | alignleft aligncenter ' +
+                            'alignright alignjustify | bullist numlist outdent indent | ' +
+                            'removeformat | help',
+                            content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+                        }}
+                        className='editor-tiny'
+                    />
+                    <button onClick={log}>Enviar</button>
+
+                    <br/>
+
+                    <select className='pn-menu-options'>
+                        <option value="">Tipo de Postagem</option>
+                        <option value="">Aviso</option>
+                        <option value="">Noticia</option>
+                        <option value="">Gremio</option>
+                        <option value="">Monitoria</option>
+                    </select>   
                 </div>
             </div>
        </> 
