dd22017954a4e8939e0da355013c3cecb2952f5a
TeraWill <newmeta.will@gmail.com>
2022-06-15 15:22:52 -0300

    Colocando as informações no Sobrenos

--- src/pages/sobrenos/index.js
+++ src/pages/sobrenos/index.js
@@ -18,11 +18,11 @@ function Sobrenos(){
 
                 <div className="container between">
                     <div className = "content-box content-right">
-                        <input type= "checkbox" id="cb-show"/>
+                        
                         <label htmlFor="cb-show"><div className = "img"/></label> 
-
+                        <input type= "checkbox" id="cb-show"/>
                         <div className="dev-info">
-                            <h1>William Vinicius Ayres de Paula </h1> <p>Parte Desenvolvida</p>
+                            <h1>Ana Paula Rocha da Silva  </h1> <p> DBA e Documentalista</p>
                             <div id = "hidden-info">
                                 <ul>
                                     <li>Informações Extras</li>
@@ -39,7 +39,7 @@ function Sobrenos(){
                         <input type= "checkbox" id="cb-show2"/>
 
                         <div className="dev-info">
-                            <h1>Ana Paula Rocha de Souza </h1> <p>Parte Desenvolvida</p>
+                            <h1>Cauã da Silva Cruz </h1> <p>Full stack Mobile</p>
                             <div id = "hidden-info2">
                                 <ul>
                                     <li>Informações Extras</li>
@@ -55,11 +55,11 @@ function Sobrenos(){
 
                 <div className="container between">
                     <div className = "content-box content-right">
-                        <input type= "checkbox" id="cb-show3"/>
                         <label htmlFor="cb-show3"><div className = "img"/></label> 
 
+                        <input type= "checkbox" id="cb-show3"/>
                         <div className="dev-info">
-                            <h1>Cezar Adriano </h1> <p>Parte Desenvolvida</p>
+                            <h1>Cesar Adriano de Freitas </h1> <p>Full stack Mobile</p>
                             <div id = "hidden-info3">
                                 <ul>
                                     <li>Informações Extras</li>
@@ -75,7 +75,7 @@ function Sobrenos(){
                     <div className = "content-box content-left">
                         <input type= "checkbox" id="cb-show4"/>
                         <div className="dev-info">
-                            <h1>Gizele Cristina </h1> <p>Parte Desenvolvida</p>
+                            <h1>Diogo Correia Campos </h1> <p>Full stack mobile e DBA</p>
                             <div id = "hidden-info4">
                                 <ul>
                                     <li>Informações Extras</li>
@@ -85,8 +85,97 @@ function Sobrenos(){
                             </div>
                         </div>
 
-                       
                         <label htmlFor="cb-show4"><div className = "img"/></label> 
+                    </div>
+                </div>
+                <div className="container between">
+                    <div className = "content-box content-right">
+                        
+                        <label htmlFor="cb-show5"><div className = "img"/></label> 
+                        <input type= "checkbox" id="cb-show5"/>
+                        <div className="dev-info">
+
+                            <h1>Diogo Dias Rodrigues Gallina </h1> <p>Front-end e Designer mobile</p>
+                            <div id = "hidden-info5">
+                                <ul>
+                                    <li>Informações Extras</li>
+                                    <li> <a href="#"> <div className="mini-icon"/> LinkedIn</a><br/></li>
+                                    <li> <a href="#"> <div className="mini-icon"/> GitHub</a><br/></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="container between">
+                    <div className = "content-box content-left">
+                        <input type= "checkbox" id="cb-show6"/>
+
+                        <div className="dev-info">
+                            <h1>Erik Alves da Silva </h1> <p>Front end Mobile e Web</p>
+                            <div id = "hidden-info6">
+                                <ul>
+                                    <li>Informações Extras</li>
+                                    <li> <a href="#"> LinkedIn <div className="mini-icon"/></a><br/></li>
+                                    <li> <a href="#"> GitHub <div className="mini-icon"/> </a><br/></li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <label htmlFor="cb-show6"><div className = "img"/></label> 
+                    </div>
+                </div>
+                <div className="container between">
+                    <div className = "content-box content-right">
+                        
+                        <label htmlFor="cb-show7"><div className = "img"/></label> 
+                        <input type= "checkbox" id="cb-show7"/>
+                        <div className="dev-info">
+                            <h1>Gisele Cristina dos Santos </h1> <p>Tester e Documentalista</p>
+                            <div id = "hidden-info7">
+                                <ul>
+                                    <li>Informações Extras</li>
+                                    <li> <a href="#"> <div className="mini-icon"/> LinkedIn</a><br/></li>
+                                    <li> <a href="#"> <div className="mini-icon"/> GitHub</a><br/></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="container between">
+                    <div className = "content-box content-left">
+                        <input type= "checkbox" id="cb-show8"/>
+
+                        <div className="dev-info">
+                            <h1>Paulo Cezar da Silva Ribeiro </h1> <p>Arquiteto de Cybersegurança</p>
+                            <div id = "hidden-info8">
+                                <ul>
+                                    <li>Informações Extras</li>
+                                    <li> <a href="#"> LinkedIn <div className="mini-icon"/></a><br/></li>
+                                    <li> <a href="#"> GitHub <div className="mini-icon"/> </a><br/></li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <label htmlFor="cb-show8"><div className = "img"/></label> 
+                    </div>
+                </div>
+                <div className="container between">
+                    <div className = "content-box content-right">
+                        
+                        <label htmlFor="cb-show9"><div className = "img"/></label> 
+                        <input type= "checkbox" id="cb-show9"/>
+                        <div className="dev-info">
+                            <h1>William Vinicius Ayres de Paula </h1> <p>Front-end web e Documentalista</p>
+                            <div id = "hidden-info9">
+                                <ul>
+                                    <li>Informações Extras</li>
+                                    <li> <a href="#"> <div className="mini-icon"/> LinkedIn</a><br/></li>
+                                    <li> <a href="#"> <div className="mini-icon"/> GitHub</a><br/></li>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
